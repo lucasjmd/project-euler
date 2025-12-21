cdb390bff0f9253154db73d46a4ba2a792646e0d
lucasjmd <lucas.deijkers@proton.me>
2025-12-21 14:50:56 +0100

    Improved performance by using sets instead of lists

--- problem-003.py
+++ problem-003.py
@@ -3,27 +3,27 @@ from math import sqrt, floor
 def find_primes_under_num(num):
     # Sieve of Eratosthenes
     p = 2
-    primes = []
-    marked = []
+    primes_set = set()
+    marked_set = set()
 
     num = floor(sqrt(num))
 
     while p <= num:
-        primes.append(p)
+        primes_set.add(p)
         for i in range(p,num+1,p):
-            if i not in marked:
-                marked.append(i)
+            if i not in marked_set:
+                marked_set.add(i)
 
         for j in range(p,num+1):
-            if j not in marked:
+            if j not in marked_set:
                 p = j
                 break
             else:
                 p += 1
 
-    return primes
+    return primes_set
 
-def find_prime_factors(num):
+def find_max_prime_factor(num):
 
     primes_under_number = find_primes_under_num(num)
 
@@ -34,8 +34,8 @@ def find_prime_factors(num):
             factors.append(prime)
             num /= prime
 
-    return factors
+    return max(factors)
 
-print(max(find_prime_factors(13195)))
+print(find_max_prime_factor(600851475143))
 
 # Low performance, requires optimising.
