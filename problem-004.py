b26a0bd6ad4e8dfc0900f3f22376b80bcd07b02d
lucasjmd <lucas.deijkers@proton.me>
2025-12-22 21:25:15 +0100

    First attempt at solution

---
+++ problem-004.py
@@ -0,0 +1,24 @@
+current_max_factors = [None, None]
+
+for factor_1 in range(100,1000):
+    for factor_2 in range(100,1000):
+
+        product = factor_1 * factor_2
+
+        product = list(str(product))
+
+
+        digits_in_product = len(product)
+
+        for i in range((len(product)//2)):
+            if product[i] == product.pop():
+                continue
+            else:
+                break
+
+        else:
+            current_max_factors[0] = factor_1
+            current_max_factors[1] = factor_2
+
+print(current_max_factors)
+print(current_max_factors[0]*current_max_factors[1])
